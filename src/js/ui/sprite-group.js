09117722694e48a47d2cebfc868d0ce04006927c
Nikolas McGovern <nmcgove@ncsu.edu>
2017-09-01 11:50:52 -0400

    Add documentation

--- src/js/ui/sprite-group.js
+++ src/js/ui/sprite-group.js
@@ -1,20 +1,34 @@
 /**
- *
+ * Creates a sprite group.
  * @constructor
  */
 function SpriteGroup() {
   this.sprites = [];
 
+  /**
+   * Adds the sprite to the group.
+   * @param sprite {Sprite} The sprite to add.
+   */
   this.add = function(sprite) {
     'use strict';
     this.sprites.push(sprite);
   };
 
+  /**
+   * Removes the sprite from the group.
+   * @param index {number} The index to remove.
+   * @returns {Sprite} The sprite removed.
+   */
   this.removeIndex = function(index) {
     'use strict';
-    return this.sprites.splice(index, 1);
+    return this.sprites.splice(index, 1)[0];
   };
 
+  /**
+   * Removes the sprite from the group.
+   * @param sprite {Sprite} The sprite to remove.
+   * @returns {Sprite} The sprite removed or <code>null</code> if the sprite was not part of the group.
+   */
   this.removeSprite = function(sprite) {
     'use strict';
     var index = this.sprites.indexOf(sprite);
@@ -24,11 +38,21 @@ function SpriteGroup() {
     return null;
   };
 
+  /**
+   * Returns the number of sprites in the group.
+   * @returns {Number} the number of sprites in the group.
+   */
   this.size = function() {
     'use strict';
     return this.sprites.length;
   };
 
+  /**
+   * Returns the sprite at the given coordinates.
+   * @param x {Number} The x-coordinate.
+   * @param y {Number} The y-coordinate.
+   * @returns {Sprite} The sprite at the coordinates or <code>null</code> if there is no sprite at the coordinates.
+   */
   this.getSprite = function(x, y) {
     'use strict';
     console.log('Get Sprite...');
@@ -42,6 +66,11 @@ function SpriteGroup() {
     return null;
   };
 
+  /**
+   * Returns the sprite overlapping with the given sprite.
+   * @param sprite {Sprite} The sprite to check against.
+   * @returns {Sprite} The overlapping sprite or <code>null</code> if there is no overlapping sprite.
+   */
   this.getOverlap = function(sprite) {
     'use strict';
     console.log('Get Overlap...');
