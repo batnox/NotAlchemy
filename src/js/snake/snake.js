f0b2b345db802ddac5abf17b7c494a90ff044c97
mwprice <mwprice@ncsu.edu>
2017-09-14 17:49:02 -0400

    Update snake.js

--- src/js/snake/snake.js
+++ src/js/snake/snake.js
@@ -1,5 +1,36 @@
-class Snake {
+class Snake extends Game {
   constructor() {
+    super();
+    /**
+     * Determines the direction that the Snake will go
+     */
+    addEventListener('keydown', event => this.onKeyDown(event));
+    const Directions = Object.freeze({
+    UP:    Symbol("up"),
+    DOWN:  Symbol("down"),
+    LEFT:  Symbol("left"),
+    RIGHT: Symbol("right")
+  });
+  }
+  
 
+  /**
+   * Handles changing the snake's direction based on user input. 
+   */
+  onKeyDown(event) {
+    'use strict';
+    //up
+    if(event.keyCode == '38') {
+      this.setDirection(Directions.UP);
+    //down
+    } else if (event.keyCode == '40') {
+     this.setDirection(Directions.DOWN);
+    //left
+    } else if (event.keyCode == '36') {
+      this.setDirection(Directions.LEFT);
+    //right
+    } else if (event.keyCode == '37') {
+      this.setDirection(Directions.RIGHT);
+    }
   }
 }
