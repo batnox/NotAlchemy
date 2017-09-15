05badc31d38ba46ec5f9a35034a1b70221fbbd2d
Wan-Yi Yeh <wyeh2@ncsu.edu>
2017-09-15 16:01:03 -0400

    random choose wall materials(different level has different materials)

--- src/js/snake/levelController.js
+++ src/js/snake/levelController.js
@@ -1,9 +1,9 @@
 class Level extends Game{
     constructor() {
         super();
-        this.GRID_NUMBER = 40;
-        this.GRID_SIZE = 10;
-        this.currentLevel = 0;//0 and 1
+        this.GRID_NUMBER = 30;
+        this.GRID_SIZE = 40;
+        this.currentLevel = 1;//0 and 1
         this.addContent('snake_elements', 'json/snake_elements.json');
         this.wallMaterial = [];
         //this.wallSprites = [];
@@ -13,9 +13,11 @@ class Level extends Game{
         this.canvas.width = this.GRID_NUMBER* this.GRID_SIZE;
         this.canvas.height = this.GRID_NUMBER* this.GRID_SIZE;
         this.loadContent().then(()=>this.map());
+    }
 
-
-
+    returnAllSprites(){
+        console.log(this.sprites);
+        return this.sprites;
     }
 
     drawWall() {
@@ -43,14 +45,15 @@ class Level extends Game{
             for (let y = 0; y < this.GRID_NUMBER; y++) {
                 if (x === 0 || y === 0 || x === this.GRID_NUMBER - 1 || y === this.GRID_NUMBER - 1) {
                     this.MAP1[x][y] = 1;
-                    let s = Object.assign(Object.create(Object.getPrototypeOf(this.wallMaterial[0])), this.wallMaterial[0]);
-                    s.setPosition(x*this.GRID_SIZE, y*this.GRID_SIZE);//error here
-                    this.sprites.push(s);
-                    /*{
-                        let tmpSprite = this.wallMaterial[0][Math.floor(Math.random() * 4)];
+                    //let s = Object.assign(Object.create(Object.getPrototypeOf(this.wallMaterial[0])), this.wallMaterial[0]);
+                    //s.setPosition(x*this.GRID_SIZE, y*this.GRID_SIZE);//error here
+                    //this.sprites.push(s);
+                    {
+                        let random = Math.floor(Math.random() * 4);
+                        let tmpSprite = Object.assign(Object.create(Object.getPrototypeOf(this.wallMaterial[this.currentLevel][random])), (this.wallMaterial[this.currentLevel][random]));
                         tmpSprite.setPosition(x * this.GRID_SIZE, y * this.GRID_SIZE);
-                        this.wallSprites[this.wallSprites.length - 1].add(tmpSprite);
-                    }*/
+                        this.sprites.push(tmpSprite);
+                    }
                 } else {
                     this.MAP1[x][y] = 0;
                 }
@@ -64,40 +67,43 @@ class Level extends Game{
             for (let y = 0; y < this.GRID_NUMBER; y++) {
                 if (x === 0 || y === 0 || x === (this.GRID_NUMBER - 1) || y === (this.GRID_NUMBER - 1)) {
                     this.MAP2[x][y] = 1;
-                    /*{
-                        let tmpSprite = this.wallMaterial[1][Math.floor(Math.random() * 4)];
+                    {
+                        let random = Math.floor(Math.random() * 4);
+                        let tmpSprite = Object.assign(Object.create(Object.getPrototypeOf(this.wallMaterial[this.currentLevel][random])), (this.wallMaterial[this.currentLevel][random]));
                         tmpSprite.setPosition(x * this.GRID_SIZE, y * this.GRID_SIZE);
-                        this.wallSprites[this.wallSprites.length - 1].add(tmpSprite);
-                    }*/
+                        this.sprites.push(tmpSprite);
+                    }
                 } else if (x > Math.floor((this.GRID_NUMBER-1)/3) && x < Math.floor((this.GRID_NUMBER-1)*2/3) &&
                     y === Math.floor((this.GRID_NUMBER)/2)){
                     this.MAP2[x][y] = 1;
-                    /*{
-                        let tmpSprite = this.wallMaterial[1][Math.floor(Math.random() * 4)];
+                    {
+                        let random = Math.floor(Math.random() * 4);
+                        let tmpSprite = Object.assign(Object.create(Object.getPrototypeOf(this.wallMaterial[this.currentLevel][random])), (this.wallMaterial[this.currentLevel][random]));
                         tmpSprite.setPosition(x * this.GRID_SIZE, y * this.GRID_SIZE);
-                        this.wallSprites[this.wallSprites.length - 1].add(tmpSprite);
-                    }*/
+                        this.sprites.push(tmpSprite);
+                    }
                 }
                 else if (y > Math.floor((this.GRID_NUMBER-1)/3) && y < Math.floor((this.GRID_NUMBER-1)*2/3) &&
                     x === Math.floor((this.GRID_NUMBER)/2)){
                     this.MAP2[x][y] = 1;
-                    /*{
-                        let tmpSprite = this.wallMaterial[1][Math.floor(Math.random() * 4)];
+                    {
+                        let random = Math.floor(Math.random() * 4);
+                        let tmpSprite = Object.assign(Object.create(Object.getPrototypeOf(this.wallMaterial[this.currentLevel][random])), (this.wallMaterial[this.currentLevel][random]));
                         tmpSprite.setPosition(x * this.GRID_SIZE, y * this.GRID_SIZE);
-                        this.wallSprites[this.wallSprites.length - 1].add(tmpSprite);
-                    }*/
+                        this.sprites.push(tmpSprite);
+                    }
                 }
                 else {
                     this.MAP2[x][y] = 0;
                 }
             }
         }
-/*
-        let s = this.wallMaterial[0];
-        console.log(JSON.stringify(this.wallMaterial));
-        s.setSize(5, 5);//error here
-        this.sprites.push(s);
-        */
+        /*
+                let s = this.wallMaterial[0];
+                console.log(JSON.stringify(this.wallMaterial));
+                s.setSize(5, 5);//error here
+                this.sprites.push(s);
+                */
         this.draw();
 
         //this.drawWall();
@@ -111,18 +117,19 @@ class Level extends Game{
         return super.loadContent()
             .then(data => {
                 for ( let i in data['snake_elements'].walls){
-                    //this.wallMaterial[i] = [];
+                    this.wallMaterial[i] = [];
                     for (let j in  data['snake_elements'].walls[i].material) {
+                        /*
                         let tmp = new Sprite();
-                        //console.log(data['snake_elements'].walls[i].material[j]);
                         tmp.setImage(data['snake_elements'].walls[i].material[j]);
                         tmp.setSize(this.GRID_SIZE, this.GRID_SIZE);
                         this.wallMaterial.push(tmp);
+                        */
                         //console.log(JSON.stringify(this.wallMaterial));
                         //console.log(data['snake_elements'].walls[i].material[j]);
-                        //this.wallMaterial[i][j] = new Sprite();
-                        //this.wallMaterial[i][j].setImage(data['snake_elements'].walls[i].material[j]);
-                        //this.wallMaterial[i][j].setSize(this.GRID_SIZE, this.GRID_SIZE);
+                        this.wallMaterial[i][j] = new Sprite();
+                        this.wallMaterial[i][j].setImage(data['snake_elements'].walls[i].material[j]);
+                        this.wallMaterial[i][j].setSize(this.GRID_SIZE, this.GRID_SIZE);
                         //console.log(data['snake_elements'].walls[i].material[j]);
                     }
                 }
