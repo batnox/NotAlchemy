98e48c2164ee3d47843494d66b73b5249e4723cf
mwprice <mwprice@ncsu.edu>
2017-09-15 15:17:10 -0400

    Initialized Update and
    Gave potential solution to keep track of current direction and began work in update

--- src/js/snake/snake.js
+++ src/js/snake/snake.js
@@ -2,6 +2,8 @@ class Snake extends Game {
   constructor() {
     super();
     this.worm = new SnakeSegment();
+    this.currentDirection = "right";
+    this.scorecount = 0;
     /**
      * Determines the direction that the Snake will go
      */
@@ -17,21 +19,31 @@ class Snake extends Game {
     //up
     if(event.keyCode == '38') {
       this.setDirection(Directions.UP);
+      this.currentDirection = "up";
     //down
     } else if (event.keyCode == '40') {
      this.setDirection(Directions.DOWN);
+      this.currentDirection = "down";
     //left
     } else if (event.keyCode == '37') {
       this.setDirection(Directions.LEFT);
+      this.currentDirection = "left";
     //right
     } else if (event.keyCode == '39') {
       this.setDirection(Directions.RIGHT);
+      this.currentDirection = "right";
     }
   }
   
   update() {
     super.update();
-    worm.moveSegment();
+    worm.moveSegment(currentDirection, 10);
+    if(//Collides with food) {
+      worm.addlink(10);
+      scorecount++;
+    } else if( //collides with wall or collides with self // ) {
+      //Score message
+    }
   }
   
   draw() {
