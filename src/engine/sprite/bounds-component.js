25ac9eba12e5a92353c98c882aa0760d1c8606d5
Nikolas McGovern <nmcgove@ncsu.edu>
2017-10-03 11:48:51 -0400

    Separate sprite into bounds and image components

---
+++ src/engine/sprite/bounds-component.js
@@ -0,0 +1,35 @@
+class BoundsComponent {
+  constructor() {
+    this.x = 0;
+    this.y = 0;
+    this.width = 0;
+    this.height = 0;
+    this.rotation = 0;
+  }
+
+  setPosition(x, y) {
+    this.x = x;
+    this.y = y;
+  }
+
+  setSize(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  setRotation(rotation) {
+    this.rotation = rotation;
+  }
+
+  contains(x, y) {
+    return this.x <= x && x < this.x + this.width
+      && this.y <= y && y < this.y + this.height;
+  }
+
+  isCollision(sprite) {
+    return this.x < sprite.bounds.x + sprite.bounds.width
+      && this.x + this.width > sprite.bounds.x
+      && this.y < sprite.bounds.y + sprite.bounds.height
+      && this.y + this.height > sprite.bounds.y;
+  }
+}
---
+++ src/engine/sprite/sprite.js
@@ -0,0 +1,27 @@
+class Sprite {
+  /**
+   * Creates a sprite.
+   * @constructor
+   */
+  constructor() {
+    /**
+     * The bounds of the sprite.
+     * @type {BoundsComponent}
+     */
+    this.bounds = null;
+    /**
+     * The image of the sprite.
+     * @type {ImageComponent}
+     */
+    this.image = null;
+  }
+
+  /**
+   * @param context {CanvasRenderingContext2D}
+   */
+  draw(context) {
+    if (this.image) {
+      this.image.draw(context);
+    }
+  }
+}
